0272ca376733f817df338b5cb33d2cc4dc06b2f9
Samuel Tardif <samhockey04@hotmail.com>
2021-11-29 12:14:04 -0500

    Fixed health bar

---
+++ LabEscape/Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,31 @@
+﻿// ------------------------------------------------------------------------------
+// Quiz
+// Written by: Samuel Tardif iD:40051573
+// For COMP 376 – Fall 2021
+// ----------------------------------------------------------------------------- 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+
+	//Code taken from : https://www.youtube.com/watch?v=BLfNP4Sc_iA&ab_channel=Brackeys
+	//As well as health bar sprites
+
+
+	public Slider slider;
+	public GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update()
+    {
+		slider.value = player.GetComponent<PlayerSam>().health;
+	}
+}
